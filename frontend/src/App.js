63b8e9e48826cfde65940a7e4210521f1f1c590f
Ankan <ankanbanerjee12345@gmail.com>
2025-03-26 16:01:00 +0530

    react src add

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>LegalLens - Contract Management</h1>
+        <p>
+          Welcome to LegalLens, your intelligent contract management solution.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App; 
